03acd05c3012d7e5b90c2c514a37951bbfb1b3af
Max Sonderegger <maxmsonderegger@gmail.com>
2019-03-05 20:35:14 -0500

    Initial commit

--- Whitakers Word Helper Extension/SafariExtensionHandler.swift
+++ Whitakers Word Helper Extension/SafariExtensionHandler.swift
@@ -12,9 +12,44 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        page.getPropertiesWithCompletionHandler { properties in
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
-        }
+        var formattedText = messageName.replacingOccurrences(of: "ā", with: "a")
+        formattedText = formattedText.replacingOccurrences(of: "ē", with: "e")
+        formattedText = formattedText.replacingOccurrences(of: "ī", with: "i")
+        formattedText = formattedText.replacingOccurrences(of: "ō", with: "o")
+        formattedText = formattedText.replacingOccurrences(of: "ū", with: "u")
+
+        let url = URL(string: "http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=\(formattedText)")!
+        let urlSession = URLSession.shared
+        
+        let getRequest = URLRequest(url: url)
+        
+        let task = urlSession.dataTask(with: getRequest as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : error!.localizedDescription])
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            // the data is returned in JSON format and needs to be converted into something that swift can work with
+            // we are converting it into a dictionary of type [String: Any]
+            guard let responseText = String(bytes: data, encoding: .utf8) else {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error decoding URL response"])
+                return
+            }
+            let firstTagIndex = responseText.range(of: "<pre>")?.upperBound
+            let lastTagIndex = responseText.range(of: "</pre>")?.lowerBound
+            let selectedText = String(responseText[firstTagIndex!..<lastTagIndex!])
+            if selectedText.isEmpty {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error parsing URL response"])
+                return
+            }
+            page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : selectedText])
+        })
+        
+        task.resume()
     }
     
     override func toolbarItemClicked(in window: SFSafariWindow) {
--- Whitakers Word Helper Extension/script.js
+++ Whitakers Word Helper Extension/script.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+                          var pop_up_div = document.createElement("DIV");
+                          pop_up_div.id = "WWH_pop_up_div";
+                          var internal_div = document.createElement("DIV");
+                          internal_div.id = "WWH_internal_div"
+                          var pre_text = document.createElement("PRE");
+                          internal_div.appendChild(pre_text);
+                          pop_up_div.appendChild(internal_div);
+                          
+                          $( "body" ).append(pop_up_div);
 });
+
+document.addEventListener("click", function(event) {
+                          document.getElementById("WWH_pop_up_div").style.display = 'none';
+});
+
+document.addEventListener("dblclick", function(event) {
+    if (window.getSelection()) {
+        var text = window.getSelection().toString();
+        if (text != "") {
+            $( "#WWH_pop_up_div" ).css({'display' : 'inline-block'});
+            $( "#WWH_pop_up_div" ).offset({left:event.pageX,top:event.pageY});
+            $( "#WWH_pop_up_div #WWH_internal_div pre" ).text("");
+            safari.extension.dispatchMessage(text);
+        }
+    }
+});
+
+safari.self.addEventListener("message", extensionMessageHandler);
+
+function extensionMessageHandler(event) {
+    if (event.name === "queryResponse") {
+        $( "#WWH_pop_up_div #WWH_internal_div pre" ).text(event.message["responseText"]);
+        var pop_up_div = document.getElementById("WWH_pop_up_div");
+        var div_rect = pop_up_div.getBoundingClientRect();
+        if (div_rect.x + div_rect.width > document.documentElement.clientWidth) {
+            pop_up_div.style.left = (document.documentElement.clientWidth - div_rect.width - 5).toString() + "px";
+        }
+        if (div_rect.y + div_rect.height > document.documentElement.clientHeight) {
+            pop_up_div.style.top = (document.documentElement.clientHeight - div_rect.height + window.scrollY - 5).toString() + "px";
+        }
+    }
+}
