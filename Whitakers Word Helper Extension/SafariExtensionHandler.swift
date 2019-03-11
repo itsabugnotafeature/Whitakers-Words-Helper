4b669f181c1c33bf53bebeced7cbca5005a7b8dc
Max Sonderegger <maxmsonderegger@gmail.com>
2019-03-11 10:24:32 -0400

    Moved pop up structure to ShadowDOM, removed old dependencies

--- Whitakers Word Helper Extension/SafariExtensionHandler.swift
+++ Whitakers Word Helper Extension/SafariExtensionHandler.swift
@@ -12,44 +12,16 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        var formattedText = messageName.replacingOccurrences(of: "ā", with: "a")
-        formattedText = formattedText.replacingOccurrences(of: "ē", with: "e")
-        formattedText = formattedText.replacingOccurrences(of: "ī", with: "i")
-        formattedText = formattedText.replacingOccurrences(of: "ō", with: "o")
-        formattedText = formattedText.replacingOccurrences(of: "ū", with: "u")
-
-        let url = URL(string: "http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=\(formattedText)")!
-        let urlSession = URLSession.shared
-        
-        let getRequest = URLRequest(url: url)
-        
-        let task = urlSession.dataTask(with: getRequest as URLRequest, completionHandler: { data, response, error in
-            
-            guard error == nil else {
-                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : error!.localizedDescription])
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            // the data is returned in JSON format and needs to be converted into something that swift can work with
-            // we are converting it into a dictionary of type [String: Any]
-            guard let responseText = String(bytes: data, encoding: .utf8) else {
-                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error decoding URL response"])
-                return
-            }
-            let firstTagIndex = responseText.range(of: "<pre>")?.upperBound
-            let lastTagIndex = responseText.range(of: "</pre>")?.lowerBound
-            let selectedText = String(responseText[firstTagIndex!..<lastTagIndex!])
-            if selectedText.isEmpty {
-                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error parsing URL response"])
-                return
-            }
-            page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : selectedText])
-        })
-        
-        task.resume()
+        doDefinitions(withQuery: messageName, from: page, userInfo: userInfo)
+    }
+    
+    override func contextMenuItemSelected(withCommand command: String, in page: SFSafariPage, userInfo: [String : Any]? = nil) {
+        switch command {
+        case "searchRequest":
+            doDefinitions(withQuery: userInfo!["selectedText"] as! String, from: page, userInfo: userInfo)
+        default:
+            return
+        }
     }
     
     override func toolbarItemClicked(in window: SFSafariWindow) {
@@ -65,5 +37,69 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func popoverViewController() -> SFSafariExtensionViewController {
         return SafariExtensionViewController.shared
     }
+    
+    func doDefinitions(withQuery queryString: String, from page: SFSafariPage, userInfo: [String : Any]?) {
+        var formattedText = queryString.replacingOccurrences(of: "ā", with: "a")
+        formattedText = formattedText.replacingOccurrences(of: "ē", with: "e")
+        formattedText = formattedText.replacingOccurrences(of: "ī", with: "i")
+        formattedText = formattedText.replacingOccurrences(of: "ō", with: "o")
+        formattedText = formattedText.replacingOccurrences(of: "ū", with: "u")
+        formattedText = formattedText.replacingOccurrences(of: "Ā", with: "A")
+        formattedText = formattedText.replacingOccurrences(of: "Ē", with: "E")
+        formattedText = formattedText.replacingOccurrences(of: "Ī", with: "I")
+        formattedText = formattedText.replacingOccurrences(of: "Ō", with: "O")
+        formattedText = formattedText.replacingOccurrences(of: "Ū", with: "U")
+        formattedText = formattedText.replacingOccurrences(of: " ", with: "+")
+        
+        let numberOfWords = formattedText.split(separator: "+").count
+        if numberOfWords == 2 {
+            formattedText += "+et"
+        }
+        
+        let url = URL(string: "http://www.archives.nd.edu/cgi-bin/wordz.pl?keyword=\(formattedText)")!
+        let urlSession = URLSession.shared
+        
+        let getRequest = URLRequest(url: url)
+        
+        let task = urlSession.dataTask(with: getRequest as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : error!.localizedDescription])
+                return
+            }
+            
+            guard let data = data else {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error looking up data\nError code: \(WhitakersWordsHelperErrors.dataNotReturned.rawValue)"])
+                return
+            }
+            // the data is returned in JSON format and needs to be converted into something that swift can work with
+            // we are converting it into a dictionary of type [String: Any]
+            guard let responseText = String(bytes: data, encoding: .utf8) else {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error decoding URL response\nError code: \(WhitakersWordsHelperErrors.cannotDecodeData.rawValue)"])
+                return
+            }
+            let firstTagIndex = responseText.range(of: "<pre>")?.upperBound
+            let lastTagIndex = responseText.range(of: "</pre>")?.lowerBound
+            var selectedText = String(responseText[firstTagIndex!..<lastTagIndex!])
+            if selectedText.isEmpty {
+                page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : "Error parsing URL response\nError code: \(WhitakersWordsHelperErrors.parsingFailed.rawValue)"])
+                return
+            }
+            if numberOfWords == 2 {
+                var splitText = selectedText.split(separator: "\n", maxSplits: Int.max, omittingEmptySubsequences: false)
+                splitText.removeSubrange((splitText.count - 6)..<splitText.count - 2)
+                selectedText = splitText.joined(separator: "\n")
+            }
+            page.dispatchMessageToScript(withName: "queryResponse", userInfo: ["responseText" : selectedText])
+        })
+        
+        task.resume()
+    }
 
+}
+
+enum WhitakersWordsHelperErrors: Int {
+    case dataNotReturned
+    case cannotDecodeData
+    case parsingFailed
 }
--- Whitakers Word Helper Extension/script.js
+++ Whitakers Word Helper Extension/script.js
@@ -1,26 +1,32 @@
+var WWH_host_div = document.createElement("div");
+WWH_host_div.id = "WWH_host_div";
+var shadowRoot = WWH_host_div.attachShadow({mode: 'closed'});
+shadowRoot.innerHTML = "<style>#WWH_pop_up_div {background-color: rgb(110, 110, 110);padding: 2px !important;border-radius: 10px !important;position: absolute !important;visibility: hidden;min-width: 450px;z-index: 100;opacity: 1 !important;font-family: Consolas, monospace !important;}#WWH_internal_div {background-color: rgb(255, 255, 255);border-radius: 15px !important;padding-left: 5px !important;padding-right: 5px !important;}#WWH_pop_up_div pre {font-family: Consolas, monospace !important;font-size: 14px !important;padding: 0 !important;margin: 0 !important;background-color: rgb(255, 255, 255, 0) !important;color: rgb(0, 0, 0) !important;box-shadow: none !important;line-height: 100% !important;}</style>"
+var pop_up_div = document.createElement("div");
+pop_up_div.id = "WWH_pop_up_div";
+var internal_div = document.createElement("div");
+internal_div.id = "WWH_internal_div"
+var pre_text = document.createElement("pre");
+internal_div.appendChild(pre_text);
+pop_up_div.appendChild(internal_div);
+shadowRoot.appendChild(pop_up_div);
+
 document.addEventListener("DOMContentLoaded", function(event) {
-                          var pop_up_div = document.createElement("DIV");
-                          pop_up_div.id = "WWH_pop_up_div";
-                          var internal_div = document.createElement("DIV");
-                          internal_div.id = "WWH_internal_div"
-                          var pre_text = document.createElement("PRE");
-                          internal_div.appendChild(pre_text);
-                          pop_up_div.appendChild(internal_div);
-                          
-                          $( "body" ).append(pop_up_div);
+                          document.body.appendChild(WWH_host_div);
 });
 
 document.addEventListener("click", function(event) {
-                          document.getElementById("WWH_pop_up_div").style.display = 'none';
+                          pop_up_div.style.visibility = 'hidden';
 });
 
 document.addEventListener("dblclick", function(event) {
-    if (window.getSelection()) {
+    if (window.getSelection() && document.activeElement.tagName.toLowerCase() != "input" && document.activeElement.tagName.toLowerCase() != "textarea") {
         var text = window.getSelection().toString();
         if (text != "") {
-            $( "#WWH_pop_up_div" ).css({'display' : 'inline-block'});
-            $( "#WWH_pop_up_div" ).offset({left:event.pageX,top:event.pageY});
-            $( "#WWH_pop_up_div #WWH_internal_div pre" ).text("");
+            pop_up_div.style.visibility = 'visible';
+            pop_up_div.style.left = (event.pageX + 15).toString() + "px";
+            pop_up_div.style.top = event.pageY.toString() + "px";
+            pre_text.textContent = "";
             safari.extension.dispatchMessage(text);
         }
     }
@@ -30,8 +36,8 @@ safari.self.addEventListener("message", extensionMessageHandler);
 
 function extensionMessageHandler(event) {
     if (event.name === "queryResponse") {
-        $( "#WWH_pop_up_div #WWH_internal_div pre" ).text(event.message["responseText"]);
-        var pop_up_div = document.getElementById("WWH_pop_up_div");
+        pop_up_div.style.visibility = 'visible';
+        pre_text.textContent = event.message["responseText"];
         var div_rect = pop_up_div.getBoundingClientRect();
         if (div_rect.x + div_rect.width > document.documentElement.clientWidth) {
             pop_up_div.style.left = (document.documentElement.clientWidth - div_rect.width - 5).toString() + "px";
@@ -39,5 +45,16 @@ function extensionMessageHandler(event) {
         if (div_rect.y + div_rect.height > document.documentElement.clientHeight) {
             pop_up_div.style.top = (document.documentElement.clientHeight - div_rect.height + window.scrollY - 5).toString() + "px";
         }
+    }
+}
+
+document.addEventListener("contextmenu", handleContextMenu, false);
+function handleContextMenu(event) {
+    if (window.getSelection() && document.activeElement.tagName.toLowerCase() != "input" && document.activeElement.tagName.toLowerCase() != "textarea") {
+        var selectedText = window.getSelection().toString();
+        safari.extension.setContextMenuEventUserInfo(event, { "selectedText": selectedText });
+        pop_up_div.style.left = (event.pageX + 15).toString() + "px";
+        pop_up_div.style.top = event.pageY.toString() + "px";
+        pre_text.textContent = "";
     }
 }
